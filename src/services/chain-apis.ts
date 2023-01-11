b837832a3e4bbe5ea41faf0732a82a4ff89f5a1f
Andre Miras <andre.miras@gmail.com>
2023-01-11 15:41:28 +0100

    :zap: Retry fetching validator info on error

--- src/services/chain-apis.ts
+++ src/services/chain-apis.ts
@@ -10,7 +10,7 @@ const getChainRestUrl = (chainName: string): string => {
   return rest.address;
 };
 
-const getValidator = async (
+const getValidatorOnce = async (
   chainName: string,
   validatorAddress: string
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -21,6 +21,24 @@ const getValidator = async (
   handleFetchError(response);
   const { validator } = await response.json();
   return validator;
+};
+
+const getValidator = async (
+  chainName: string,
+  validatorAddress: string,
+  retry = 3
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+): Promise<Record<string, any>> => {
+  while (retry > 0) {
+    try {
+      return await getValidatorOnce(chainName, validatorAddress);
+    } catch (error) {
+      console.error(error);
+      console.log("Error fetching validator info, retrying");
+      retry--;
+    }
+  }
+  throw Error("Error fetching validator info");
 };
 
 export { getValidator };
